2a1a8a9dd164392fcca1f3796e6ccb9fe570c07b
Rob Guard <rob.guard@capitalise.com>
2020-06-09 11:39:10 +0100

    wip

---
+++ jsonspec.py
@@ -0,0 +1,5 @@
+class JsonSpec:
+    def init(self, file):
+        #load the file
+        #check schema macthes our schema e.g. right keys, required keys
+        #load the schema using ColumnDefinition, PrimaryKeyDefinition, PermissionDefinition
